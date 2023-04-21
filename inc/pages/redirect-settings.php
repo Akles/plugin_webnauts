a889b2ba6f6b58e536f4e8594bf8e9cada53815d
Akles <h1razzaraz@gmail.com>
2023-04-21 20:34:59 +0300

    redirect plugin fix script 3

--- inc/pages/redirect-settings.php
+++ inc/pages/redirect-settings.php
@@ -42,68 +42,66 @@ function plugin_redirect_settings_page()
                     </td>
                 </tr>
             </table>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row"><?php echo __('Exception Table', 'checkintravel'); ?></th>
-                    <td>
-                        <table id="exception-table" style="width:100%">
-                            <thead>
-                            <tr>
-                                <th><?php echo __('Post Type', 'checkintravel'); ?></th>
-                                <th><?php echo __('Post', 'checkintravel'); ?></th>
-                                <th><?php echo __('Actions', 'checkintravel'); ?></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            $exceptions = get_option('exceptions');
-                            if ($exceptions) {
-                                foreach ($exceptions as $exception) {
-                                    ?>
-                                    <tr>
-                                        <td>
-                                            <input type="hidden" name="exceptions[]" value="<?php echo esc_attr($exception); ?>">
-                                            <?php echo get_post_type_object(get_post_type($exception))->labels->singular_name; ?>
-                                        </td>
-                                        <td>
-                                            <?php echo get_the_title($exception); ?>
-                                        </td>
-                                        <td>
-                                            <button class="button button-secondary remove-row"><?php echo __('Remove', 'checkintravel'); ?></button>
-                                        </td>
-                                    </tr>
-                                    <?php
-                                }
-                            }
-                            ?>
-                            </tbody>
-                        </table>
-                        <button class="button button-primary" id="add-exception"><?php echo __('Add Exception', 'checkintravel'); ?></button>
-                        <div id="exception-selector" style="display:none;">
-                            <select id="post-type-selector">
-                                <option value=""><?php echo __('&mdash; Select Post Type &mdash;', 'checkintravel'); ?></option>
-                                <?php
-                                $post_types = get_post_types(['public' => true], 'objects');
-                                foreach ($post_types as $post_type) {
-                                    echo '<option value="' . $post_type->name . '">' . $post_type->labels->singular_name . '</option>';
-                                }
-                                ?>
-                            </select>
-                            <select id="post-selector" style="display:none;">
-                                <option value=""><?php echo __('&mdash; Select Post &mdash;', 'checkintravel'); ?></option>
-                            </select>
-                            <button class="button button-primary" id="confirm-selection" style="display:none;"><?php echo __('Add', 'checkintravel'); ?></button>
-                        </div>
-                    </td>
+
+            <table id="exception-table" style="width:100%">
+                <thead>
+                <tr>
+                    <th><?php echo __('Post Type', 'checkintravel'); ?></th>
+                    <th><?php echo __('Post', 'checkintravel'); ?></th>
+                    <th><?php echo __('Actions', 'checkintravel'); ?></th>
                 </tr>
+                </thead>
+                <tbody>
+                <?php
+                $exceptions = get_option('exceptions');
+                if ($exceptions) {
+                    foreach ($exceptions as $exception) {
+                        ?>
+                        <tr>
+                            <td>
+                                <input type="hidden" name="exceptions[]"
+                                       value="<?php echo esc_attr($exception); ?>">
+                                <?php echo get_post_type_object(get_post_type($exception))->labels->singular_name; ?>
+                            </td>
+                            <td>
+                                <?php echo get_the_title($exception); ?>
+                            </td>
+                            <td>
+                                <button class="button button-secondary remove-row"><?php echo __('Remove', 'checkintravel'); ?></button>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                }
+                ?>
+                </tbody>
             </table>
+            <button class="button button-primary"
+                    id="add-exception"><?php echo __('Add Exception', 'checkintravel'); ?></button>
+            <div id="exception-selector" style="display:none;">
+                <select id="post-type-selector">
+                    <option value=""><?php echo __('&mdash; Select Post Type &mdash;', 'checkintravel'); ?></option>
+                    <?php
+                    $post_types = get_post_types(['public' => true], 'objects');
+                    foreach ($post_types as $post_type) {
+                        echo '<option value="' . $post_type->name . '">' . $post_type->labels->singular_name . '</option>';
+                    }
+                    ?>
+                </select>
+                <select id="post-selector" style="display:none;">
+                    <option value=""><?php echo __('&mdash; Select Post &mdash;', 'checkintravel'); ?></option>
+                </select>
+                <button class="button button-primary" id="confirm-selection"
+                        style="display:none;"><?php echo __('Add', 'checkintravel'); ?></button>
+            </div>
             <?php submit_button(); ?>
         </form>
     </div>
     <?php
 }
 
-function plugin_register_settings() {
+function plugin_register_settings()
+{
     register_setting('redirect_settings_group', 'unauthorized_redirect', 'intval');
     register_setting('redirect_settings_group', 'no_subscription_redirect', 'intval');
     register_setting('redirect_settings_group', 'exceptions', 'plugin_sanitize_exceptions');
@@ -111,7 +109,8 @@ function plugin_register_settings() {
 
 add_action('admin_init', 'plugin_register_settings');
 
-function plugin_sanitize_exceptions($input) {
+function plugin_sanitize_exceptions($input)
+{
     if (is_array($input)) {
         return array_map('esc_url_raw', $input);
     }
@@ -120,7 +119,8 @@ function plugin_sanitize_exceptions($input) {
 
 add_action('wp_ajax_get_posts_by_post_type', 'plugin_get_posts_by_post_type');
 
-function plugin_get_posts_by_post_type() {
+function plugin_get_posts_by_post_type()
+{
     $post_type = sanitize_text_field($_POST['post_type']);
     $posts = get_posts([
         'post_type' => $post_type,
@@ -135,11 +135,12 @@ function plugin_get_posts_by_post_type() {
     wp_die();
 }
 
-function plugin_enqueue_admin_scripts($hook) {
-    if ('settings_page_redirect_settings_group' !== $hook) {
+function plugin_enqueue_admin_scripts($hook)
+{
+    if ('plugin_page_plugin_redirect_settings' !== $hook) {
         return;
     }
-    wp_enqueue_script('plugin-admin-scripts-redirect',plugin_dir_path(__DIR__) . 'src/js/redirect.js', ['jquery'], '1.0.0', true);
+    wp_enqueue_script('plugin-admin-scripts-redirect', plugin_dir_path(__DIR__) . 'src/js/redirect.js', ['jquery'], '1.0.0', true);
     wp_localize_script('plugin-admin-scripts-redirect', 'ajax_object', [
         'ajaxurl' => admin_url('admin-ajax.php')
     ]);
